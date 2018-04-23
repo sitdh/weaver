c4377218b3408ad6eb8665568a4953be37d9d3cb
Sitdhibong Laokok <sitdhibong@gmail.com>
2018-04-23 17:07:35 +0700

    Init project

---
+++ src/main/java/com/sitdh/analyzer/weaver/App.java
@@ -0,0 +1,13 @@
+package com.sitdh.analyzer.weaver;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
