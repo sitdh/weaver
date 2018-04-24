d69b3ecd65ed6faa4fe457eeda64f46c45d66e52
Sitdhibong Laokok <sitdhibong@gmail.com>
2018-04-24 18:01:09 +0700

    Add test classes

---
+++ src/test/resources/lib/com/sitdh/Greeting.java
@@ -0,0 +1,15 @@
+package com.sitdh;
+
+public class Greeting {
+
+  private String name;
+
+  public Greeting(String name) {
+    this.name = name;
+  }
+
+  public String toString() {
+    return this.name;
+  }
+
+}
